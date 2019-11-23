518415324c47961d09073a91cdc855f6f710942d
ResidenteM <00101718@uca.edu.sv>
2019-11-23 10:00:25 -0600

    entregando

---
+++ base/database.php
@@ -0,0 +1,35 @@
+<?php
+class Database 
+{
+private static $dbName = 'nombre_de_la_basededatos'; 
+private static $dbHost = 'localhost';
+private static $dbUsername = 'nombre_de_usuario';
+private static $dbUserPassword = 'contraseña';
+
+private static $cont = null;
+
+public function __construct() {
+die('Init-Función no permitida');
+}
+
+public static function connect() {
+// Permitir solo una conexión para la totalidad del acceso
+if ( null == self::$cont )
+{
+  try
+  {
+    self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+  }
+  catch(PDOException $e)
+  {
+    die($e->getMessage());
+  }
+} 
+return self::$cont;
+}
+
+public static function disconnect()
+{
+self::$cont = null;
+}
+}
